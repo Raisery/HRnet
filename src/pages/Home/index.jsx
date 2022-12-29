cdcbbf177802c62cc346729227e3e50ee1992e77
Lucas GERARD <lucas1.gerard@gmail.com>
2022-12-29 11:18:31 +0100

    add modal for home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,19 +2,45 @@ import './index.css'
 import Header from '../../components/Header'
 import NewEmployeeForm from '../../components/NewEmployeeForm'
 import { PAGES } from '../../utils/constant'
+import ReactModal from 'react-modal'
+import { useState } from 'react'
 
+ReactModal.setAppElement('#root')
 /**
  * Component for display home page
  *
  * @returns The home page
  */
 export default function Home() {
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+
+    function handleCloseModal(event) {
+        setModalIsOpen(false)
+    }
+
+    function handleOpenModal(event) {
+        setModalIsOpen(true)
+    }
     return (
         <div>
             <Header page={PAGES[0]} />
             <div className="home">
-                <NewEmployeeForm />
+                <NewEmployeeForm
+                    id="employee-form"
+                    onSubmit={handleOpenModal}
+                />
             </div>
+            <ReactModal isOpen={modalIsOpen} className="modal-content">
+                <p>
+                    Employee Created!{' '}
+                    <button
+                        className="modal-close-cross"
+                        onClick={handleCloseModal}
+                    >
+                        <i className="fas fa-times"></i>
+                    </button>
+                </p>
+            </ReactModal>
         </div>
     )
 }
